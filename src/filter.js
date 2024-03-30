48313dd17ae20d91c8188852177543e2dd05d01a
Denis Gorbunov <Gorbunosii@gmail.com>
2024-03-31 01:31:01 +0300

    2-path

--- src/filter.js
+++ src/filter.js
@@ -1,22 +1,3 @@
-const array = [
-    {
-      id: 12,
-      name: 'Name 1',
-    },
-    {
-      id: 22,
-      name: 'Name 2',
-    },
-    {
-      id: 32,
-      name: 'Name 3',
-    },
-    {
-      id: 222,
-      name: 'Name 2',
-    },
-  ];
-
 const filter = (arr, key, val) => {
     const newArr = [];
     if (key === 'name') {
